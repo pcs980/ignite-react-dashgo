03e07f98a62e548b974704424a065bec5b35963d
paulocs <paulocsmg@gmail.com>
2021-06-26 21:31:50 -0300

    Header responsiveness

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, useBreakpointValue } from '@chakra-ui/react';
 import { Logo } from './Logo';
 import { NotificationsNav } from './NotificationsNav';
 import { Profile } from './Profile';
 import { SearchBox } from './SearchBox';
 
 export function Header() {
+  const isWideScreen = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Flex
       as='header'
@@ -17,13 +22,15 @@ export function Header() {
       align='center'
     >
       <Logo />
-      <SearchBox />
+      {
+        isWideScreen && <SearchBox />
+      }
       <Flex
         align='center'
         ml='auto'
       >
         <NotificationsNav />
-        <Profile />
+        <Profile showProfileData={isWideScreen} />
       </Flex>
     </Flex>
   );
