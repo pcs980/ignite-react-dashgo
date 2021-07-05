395079b30cc738c601ec84d34881969136bd1e4d
paulocs <paulocsmg@gmail.com>
2021-07-05 18:07:38 -0300

    Install React Query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 import { DrawerProvider } from '../contexts/DrawerContext';
 import { theme } from '../styles/theme';
@@ -9,12 +11,18 @@ if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <DrawerProvider>
-        <Component {...pageProps} />
-      </DrawerProvider>
+      <QueryClientProvider client={queryClient}>
+        <DrawerProvider>
+          <Component {...pageProps} />
+        </DrawerProvider>
+
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -13,29 +13,46 @@ import {
   Thead,
   Tr,
   useBreakpointValue,
-  IconButton
+  IconButton,
+  Spinner
 } from "@chakra-ui/react";
-import { useRouter } from "next/dist/client/router";
 import Link from "next/link";
 import { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { useQuery } from 'react-query';
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
+type User = {
+  name: string;
+  email: string;
+  created_at: Date;
+};
+
 export default function UserList() {
+  const { data, error, isLoading, isFetching } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users');
+    const data = await response.json();
+
+    const users = data.users.map((user) => ({
+      ...user,
+      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      }),
+    }));
+    return users;
+  }, {
+    staleTime: 1000 * 5,
+  });
+
   const isWideScreen = useBreakpointValue({
     base: false,
     lg: true,
   });
-
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then((resp) => resp.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error(error));
-  }, []);
 
   return (
     <Box>
@@ -52,7 +69,9 @@ export default function UserList() {
         <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
           <Flex mb={['4', '4', '8']} justify='space-between' align='center'>
             <Heading size='lg' fontWeight='normal'>
-              Usuários
+              Usuários {
+                (isFetching && !isLoading) && (<Spinner size='sm' color='gray.500' ml='4' />)
+              }
             </Heading>
             <Link href='/users/create' passHref>
               <Button
@@ -67,90 +86,74 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme='whiteAlpha'>
-            <Thead>
-              <Tr>
-                <Th px={['3', '3', '6']} color='gray.300' width='8'>
-                  <Checkbox colorScheme='pink' />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideScreen && <Th>Data de cadastro</Th>}
-                <Th width={['3', '3', '6']}></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={['3', '3', '6']}>
-                  <Checkbox colorScheme='pink' />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Paulo César da Silva</Text>
-                    <Text fontSize='sm'>paulocsmg@gmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideScreen && <Td>10 de Dezembro de 2020</Td>}
-                <Td>
-                  {
-                    isWideScreen
-                      ? <Button
-                        as='a'
-                        size='sm'
-                        fontSize='sm'
-                        colorScheme='purple'
-                        leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
-                      >
-                        Editar
-                      </Button>
-                      : <IconButton
-                        size='sm'
-                        colorScheme='purple'
-                        borderRadius='6'
-                        aria-label='Edit user'
-                        icon={<Icon as={RiPencilLine} />}
-                        fontSize='16'
-                      />
-                  }
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={['3', '3', '6']}>
-                  <Checkbox colorScheme='pink' />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Paulo César da Silva</Text>
-                    <Text fontSize='sm'>paulocsmg@gmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideScreen && <Td>10 de Dezembro de 2020</Td>}
-                <Td>
-                  {
-                    isWideScreen
-                      ? <Button
-                        as='a'
-                        size='sm'
-                        fontSize='sm'
-                        colorScheme='purple'
-                        leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
-                      >
-                        Editar
-                      </Button>
-                      : <IconButton
-                        size='sm'
-                        colorScheme='purple'
-                        borderRadius='6'
-                        aria-label='Edit user'
-                        icon={<Icon as={RiPencilLine} />}
-                        fontSize='16'
-                      />
-                  }
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
+          {
+            isLoading ? (
+              <Flex justify='center'>
+                <Spinner />
+              </Flex>
+            ) : error ? (
+              <Flex justify='center'>
+                <Text>Erro ao obter usuários.</Text>
+              </Flex>
+            ) : (
+              <>
+                <Table colorScheme='whiteAlpha'>
+                  <Thead>
+                    <Tr>
+                      <Th px={['3', '3', '6']} color='gray.300' width='8'>
+                        <Checkbox colorScheme='pink' />
+                      </Th>
+                      <Th>Usuário</Th>
+                      {isWideScreen && <Th>Data de cadastro</Th>}
+                      <Th width={['3', '3', '6']}></Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    {
+                      data.map((user) => (
+                        <Tr key={user.id}>
+                          <Td px={['3', '3', '6']}>
+                            <Checkbox colorScheme='pink' />
+                          </Td>
+                          <Td>
+                            <Box>
+                              <Text fontWeight='bold'>{user.name}</Text>
+                              <Text fontSize='sm'>{user.email}</Text>
+                            </Box>
+                          </Td>
+                          {isWideScreen && <Td>{user.createdAt}</Td>}
+                          <Td>
+                            {
+                              isWideScreen
+                                ? <Button
+                                  as='a'
+                                  size='sm'
+                                  fontSize='sm'
+                                  colorScheme='purple'
+                                  leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
+                                >
+                                  Editar
+                                </Button>
+                                : <IconButton
+                                  size='sm'
+                                  colorScheme='purple'
+                                  borderRadius='6'
+                                  aria-label='Edit user'
+                                  icon={<Icon as={RiPencilLine} />}
+                                  fontSize='16'
+                                />
+                            }
+                          </Td>
+                        </Tr>
+                      ))
+                    }
+                  </Tbody>
+                </Table>
 
-          <Pagination />
+                <Pagination />
+              </>
+            )
+          }
         </Box>
       </Flex>
     </Box>
